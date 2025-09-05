db27664963fd6130590fc38a0627bd9a4736264e
Rene Calfin <rcalfino@emeal.nttdata.com>
2025-09-04 21:35:22 -0400

    analisis - create res ReActSingleInputOutputParser

--- README.md
+++ README.md
@@ -5,4 +5,19 @@ ReAct Agents combine reasoning and acting capabilities to solve complex tasks by
 ## Useful Links
 - [Langchain ReAct Documentation](https://python.langchain.com/docs/how_to/migrate_agent/)
 - [Langchain Agents Overview](https://python.langchain.com/docs/how_to/#agents)
-- [Langchain API Reference](https://python.langchain.com/api_reference/)
+- [Langchain API Reference render](https://python.langchain.com/api_reference/core/tools/langchain_core.tools.render.render_text_description.html)
+- [Tools](https://python.langchain.com/docs/concepts/tools/)
+- [Tools Kits](https://python.langchain.com/docs/integrations/tools/)
+- [Langchain hub hwchase17](https://smith.langchain.com/hub/hwchase17/react?organizationId=5c031c7d-225f-41cf-9def-21161772e1fa)
+- [ReActSingleInputOutputParser](https://python.langchain.com/api_reference/langchain/agents/langchain.agents.output_parsers.react_single_input.ReActSingleInputOutputParser.html)
+- [LCEL](https://python.langchain.com/docs/concepts/lcel/)
+
+## arguments Models
+
+```text
+    // ChatGoogleGenerativeAI
+    model_kwargs={"stop": ["\nObservation"]}  
+
+    // ChatOpenAI
+    stop= ["\nObservation"]
+```
--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 from dotenv import load_dotenv
+from langchain_core.tools import Tool, render_text_description, tool
+from langchain_core.prompts import PromptTemplate
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
 
 load_dotenv()
 
+@tool
 def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
     return len(text)
 
 def main():
     print("Hello from react-langchain!")
-    print(get_text_length("Hello, world!"))
+    
+    tools = [get_text_length]
 
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought:
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash").bind(stop=["\nObservation", "Observation"])
+    agent = { "input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser() 
+
+    res = agent.invoke({"input": "What is the length of 'DOG' in characters?"})
+    print(res)
 if __name__ == "__main__":
     main()
